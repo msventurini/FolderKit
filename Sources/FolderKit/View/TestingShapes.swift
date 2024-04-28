0ecf2bc88602570126221d7344496a379f5136df
msventurini <42521920+msventurini@users.noreply.github.com>
2024-04-28 20:39:11 -0300

    new folder path

--- Sources/FolderKit/View/TestingShapes.swift
+++ Sources/FolderKit/View/TestingShapes.swift
@@ -207,6 +207,8 @@ public struct Folder5: InsettableShape {
         self.topFolderSizeDecrease = topFolderSizeDecrease
 
     }
+
+//    deixa a pasta traseira no topo superior
 //    topFolderSizeDecrease, vai de 1 a 0.5
     public func path(in rect: CGRect) -> Path {
         
@@ -284,26 +286,129 @@ public struct Folder5: InsettableShape {
     
 }
 
+//Mudança na aba da pasta frontal para encaixar no layout
+public struct Folder6: InsettableShape {
+
+    var topSizeModifier: CGFloat
+    var insetAmount: CGFloat
+    var frontFolderTabSizeDecrease: CGFloat
+    
+    public var animatableData: Double {
+        get { frontFolderTabSizeDecrease }
+        set { frontFolderTabSizeDecrease = newValue }
+    }
+    
+    public init() {
+        self.topSizeModifier = 0
+        self.insetAmount = 0
+        self.frontFolderTabSizeDecrease = 0
+    }
+    
+    public init(topSizeModifier: CGFloat = 0, insetAmount: CGFloat = 0, frontFolderTabSizeDecrease: CGFloat = 0) {
+        self.topSizeModifier = topSizeModifier
+        self.insetAmount = insetAmount
+        self.frontFolderTabSizeDecrease = frontFolderTabSizeDecrease
+
+    }
+
+//    deixa a pasta traseira no topo superior
+//    topFolderSizeDecrease, vai de 1 a 0.5
+    public func path(in rect: CGRect) -> Path {
+        
+
+        let referenceSize: CGFloat = 350
+        let currentSize: CGFloat = (rect.width)
+        let cornerRadius: CGFloat = 24 * (currentSize/referenceSize)
+        let tabHeight: CGFloat = 48
+        let tabCornerRadius: CGFloat = 18 * (currentSize/referenceSize)
+        let textWidth: CGFloat = 82
+        let begin = CGPoint(x: rect.minX, y: rect.maxY - (cornerRadius) )
+
+        var downPath = Path()
+        downPath.move(to: begin)
+        
+        downPath.addArc(center: CGPoint(x: rect.minX + cornerRadius, y: rect.minY + cornerRadius * 3), radius: cornerRadius, startAngle: Angle(degrees: 180), endAngle: Angle(degrees: 270), clockwise: false)
+        
+        downPath.addArc(center: CGPoint(x: rect.maxX - cornerRadius*3 - textWidth, y: rect.minY + tabCornerRadius + cornerRadius/2), radius: tabCornerRadius, startAngle: Angle(degrees: 90), endAngle: Angle(degrees: 45), clockwise: true)
+        
+        downPath.addArc(center: CGPoint(x: rect.maxX - cornerRadius - textWidth, y: rect.minY + tabCornerRadius), radius: tabCornerRadius, startAngle: Angle(degrees: 225), endAngle: Angle(degrees: 270), clockwise: false)
+
+        downPath.addArc(center: CGPoint(x: rect.maxX - cornerRadius, y: rect.minY + cornerRadius), radius: cornerRadius, startAngle: Angle(degrees: 270), endAngle: Angle(degrees: 0), clockwise: false)
+        
+        downPath.addArc(
+            center:
+                CGPoint(
+                    x:
+                        rect.maxX - cornerRadius,
+                    y:
+                        rect.maxY - cornerRadius
+                ),
+            radius:
+                cornerRadius,
+            startAngle:
+                Angle(degrees: 0),
+            endAngle:
+                Angle(degrees: 90),
+            clockwise:
+                false
+        )
+        
+        downPath.addArc(
+            center:
+                CGPoint(
+                    x: rect.minX + cornerRadius,
+                    y: rect.maxY - cornerRadius
+                ),
+            radius:
+                cornerRadius,
+            startAngle:
+                Angle(degrees: 90),
+            endAngle:
+                Angle(degrees: 180),
+            clockwise: false
+        )
+        
+
+        var path3 = Path()
+        
+        path3.addPath(downPath)
+        
+//        path3.intersection(topPath)
+//        path3.addPath(topPath)
+        
+        return path3
+        
+    }
+    
+    public func inset(by amount: CGFloat) -> some InsettableShape {
+        var folder = self
+        folder.insetAmount += amount
+        return folder
+    }
+    
+    
+}
+
 struct TestingShapes: View {
     
-    @State var topFolderSize: CGFloat = 0.5
+    @State var frontFolderTabSizeDecrease: CGFloat = 0.5
     
     var body: some View {
         
-        Folder5(topFolderSizeDecrease: topFolderSize)
+        Folder6(frontFolderTabSizeDecrease: frontFolderTabSizeDecrease)
             .fill(.pink)
             .stroke(.black, lineWidth: 3)
             .frame(width: 350, height: 262)
             .background(.blue)
             .onTapGesture {
                 
-                if topFolderSize == 0.5 {
+                if frontFolderTabSizeDecrease == 0.5 {
                     withAnimation(.bouncy) {
-                        topFolderSize = 0
+                        frontFolderTabSizeDecrease = 0
                     }
                 } else {
                     withAnimation(.bouncy) {
-                        topFolderSize = 0.5
+                        frontFolderTabSizeDecrease = 0.5
                     }
                 }
                 
