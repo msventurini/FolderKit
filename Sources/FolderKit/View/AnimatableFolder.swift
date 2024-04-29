9ab31b7ca0a1270dab6f2134d17c1cd922680f0a
msventurini <42521920+msventurini@users.noreply.github.com>
2024-04-29 01:22:34 -0300

    new folder path

--- Sources/FolderKit/View/AnimatableFolder.swift
+++ Sources/FolderKit/View/AnimatableFolder.swift
@@ -24,9 +24,20 @@ public struct FrontFolder: InsettableShape {
     //Bordas inferiores da pasta
     var bottomCornerValuesProportion: CGFloat
 
+    //Muda tamanho e posição da pasta para manter dentro do frame (tentar separar)
+    var topFolderSizeDecrease: CGFloat
+
 
     
-    public init(topSizeModifier: CGFloat = 0, insetAmount: CGFloat = 0, leftInsetSize: CGFloat = 8, bottomInsetSize: CGFloat = 8, rotationValue: CGFloat = 0, bottomCornerValuesProportion: CGFloat = 0) {
+    public init(
+        topSizeModifier: CGFloat = 0,
+        insetAmount: CGFloat = 0,
+        leftInsetSize: CGFloat = 8,
+        bottomInsetSize: CGFloat = 8,
+        rotationValue: CGFloat = 0,
+        bottomCornerValuesProportion: CGFloat = 0,
+        topFolderSizeDecrease: CGFloat = 0) {
+            
         self.topSizeModifier = topSizeModifier
         self.insetAmount = insetAmount
         
@@ -36,7 +47,8 @@ public struct FrontFolder: InsettableShape {
         self.rotationValue = rotationValue
         
         self.bottomCornerValuesProportion = bottomCornerValuesProportion
-        
+            
+        self.topFolderSizeDecrease = topFolderSizeDecrease
 
     }
 
@@ -54,8 +66,8 @@ public struct FrontFolder: InsettableShape {
         
         
         
-        let begin = CGPoint(x: rect.midX, y: rect.maxY - bottomInsetSize)
-        let end = CGPoint(x: rect.midX, y: rect.maxY - bottomInsetSize)
+        let begin = CGPoint(x: rect.midX, y: rect.maxY - bottomInsetSize - (rect.height * topFolderSizeDecrease))
+        let end = CGPoint(x: rect.midX, y: rect.maxY - bottomInsetSize - (rect.height * topFolderSizeDecrease))
 
         
         
@@ -67,7 +79,7 @@ public struct FrontFolder: InsettableShape {
             center:
                 CGPoint(
                     x: rect.minX + (cornerRadius - (cornerRadius * (1 - bottomCornerValuesProportion))),
-                    y: rect.maxY - (cornerRadius - (cornerRadius * (1 - bottomCornerValuesProportion))) - bottomInsetSize
+                    y: rect.maxY - (cornerRadius - (cornerRadius * (1 - bottomCornerValuesProportion))) - bottomInsetSize - (rect.height * topFolderSizeDecrease)
                 ),
             radius:
                 cornerRadius - (cornerRadius * (1 - bottomCornerValuesProportion)),
@@ -129,8 +141,7 @@ public struct FrontFolder: InsettableShape {
                 CGPoint(
                     x:
                         rect.maxX - (cornerRadius - (cornerRadius * (1 - bottomCornerValuesProportion))) - leftInsetSize,
-                    y:
-                        rect.maxY - (cornerRadius - (cornerRadius * (1 - bottomCornerValuesProportion))) - bottomInsetSize
+                    y: rect.maxY - (cornerRadius - (cornerRadius * (1 - bottomCornerValuesProportion))) - bottomInsetSize - (rect.height * topFolderSizeDecrease)
                 ),
             radius:
                 cornerRadius - (cornerRadius * (1 - bottomCornerValuesProportion)),
@@ -148,8 +159,13 @@ public struct FrontFolder: InsettableShape {
         var path3 = Path()
         
         path3.addPath(downPath, transform: CGAffineTransformConcat(.init(scaleX: 1, y: rotationValue), .init(translationX: 0, y: rect.size.height * (1 - rotationValue))))
+//        path3.addPath(downPath)
         
-//
+        // valor inicial correto:
+        // rect.size.height * (1 - rotationValue)
+        // valor final desejado:
+        // rect.size.height * (-rotationValue)
+        
         return path3
         
     }
@@ -172,14 +188,17 @@ struct TestingShapes: View {
         var frontFolderBottomInsetSize: CGFloat = 8
         
         var frontFolderRotationValue: CGFloat = 1
+        
         var bottomCornerValuesProportion: CGFloat = 1
+        
+        var topFolderSizeDecrease: CGFloat = 0
     }
     
     var body: some View {
         
         KeyframeAnimator(initialValue: AnimationValues(), trigger: isTapped) { value in
             
-            FrontFolder(leftInsetSize: value.frontFolderLeftInsetSize, bottomInsetSize: value.frontFolderBottomInsetSize, rotationValue: value.frontFolderRotationValue, bottomCornerValuesProportion: value.bottomCornerValuesProportion)
+            FrontFolder(leftInsetSize: value.frontFolderLeftInsetSize, bottomInsetSize: value.frontFolderBottomInsetSize, rotationValue: value.frontFolderRotationValue, bottomCornerValuesProportion: value.bottomCornerValuesProportion, topFolderSizeDecrease: value.topFolderSizeDecrease)
                         .fill(.pink)
                         .stroke(.black, lineWidth: 3)
                         .frame(width: 350, height: 262)
@@ -207,6 +226,19 @@ struct TestingShapes: View {
                 CubicKeyframe(0.5, duration: 0.3)
                 CubicKeyframe(0, duration: 0.2)
             }
+            
+            KeyframeTrack(\.bottomCornerValuesProportion) {
+                CubicKeyframe(1, duration: 0.5)
+                CubicKeyframe(0.5, duration: 0.3)
+                CubicKeyframe(0, duration: 0.2)
+            }
+            
+            KeyframeTrack(\.topFolderSizeDecrease) {
+                CubicKeyframe(0, duration: 0.5)
+                CubicKeyframe(0.25, duration: 0.3)
+                CubicKeyframe(0.5, duration: 0.2)
+            }
+            
         }
 
             .onTapGesture {
--- Sources/FolderKit/View/TestingShapes.swift
+++ Sources/FolderKit/View/TestingShapes.swift
@@ -81,107 +81,107 @@ import SwiftUI
 //}
 
 //Mudança de corner radius de ambas as pastas ok
-public struct Folder4: InsettableShape {
-
-    var topSizeModifier: CGFloat
-    var insetAmount: CGFloat
-    var bottomCornerValue: CGFloat
-    
-    public var animatableData: Double {
-        get { bottomCornerValue }
-        set { bottomCornerValue = newValue }
-    }
-    
-    public init() {
-        self.topSizeModifier = 0
-        self.insetAmount = 0
-        self.bottomCornerValue = 0
-    }
-    
-    public init(topSizeModifier: CGFloat = 0, insetAmount: CGFloat = 0, bottomCornerValue: CGFloat = 0) {
-        self.topSizeModifier = topSizeModifier
-        self.insetAmount = insetAmount
-        self.bottomCornerValue = bottomCornerValue
-
-    }
-    
-    public func path(in rect: CGRect) -> Path {
-        
-
-        let referenceSize: CGFloat = 350
-        let currentSize: CGFloat = (rect.width)
-        let cornerRadius: CGFloat = 24 * (currentSize/referenceSize)
-        let tabHeight: CGFloat = 48
-        let tabCornerRadius: CGFloat = 18 * (currentSize/referenceSize)
-        let textWidth: CGFloat = 82
-        let begin = CGPoint(x: rect.minX, y: rect.maxY - cornerRadius)
-
-        var downPath = Path()
-        downPath.move(to: begin)
-        
-        downPath.addArc(center: CGPoint(x: rect.minX + cornerRadius, y: rect.minY + cornerRadius * 3), radius: cornerRadius, startAngle: Angle(degrees: 180), endAngle: Angle(degrees: 270), clockwise: false)
-        
-        downPath.addArc(center: CGPoint(x: rect.maxX - cornerRadius*3 - textWidth, y: rect.minY + tabCornerRadius + cornerRadius/2), radius: tabCornerRadius, startAngle: Angle(degrees: 90), endAngle: Angle(degrees: 45), clockwise: true)
-        
-        downPath.addArc(center: CGPoint(x: rect.maxX - cornerRadius - textWidth, y: rect.minY + tabCornerRadius), radius: tabCornerRadius, startAngle: Angle(degrees: 225), endAngle: Angle(degrees: 270), clockwise: false)
-
-        downPath.addArc(center: CGPoint(x: rect.maxX - cornerRadius, y: rect.minY + cornerRadius), radius: cornerRadius, startAngle: Angle(degrees: 270), endAngle: Angle(degrees: 0), clockwise: false)
-        
-        downPath.addArc(
-            center:
-                CGPoint(
-                    x:
-                        rect.maxX - (cornerRadius - (cornerRadius * (1 - bottomCornerValue))),
-                    y:
-                        rect.maxY - (cornerRadius - (cornerRadius * (1 - bottomCornerValue)))),
-            radius:
-                cornerRadius - (cornerRadius * (1 - bottomCornerValue)),
-            startAngle:
-                Angle(degrees: 0),
-            endAngle:
-                Angle(degrees: 90),
-            clockwise:
-                false
-        )
-        
-        downPath.addArc(
-            center:
-                CGPoint(
-                    x: rect.minX + (cornerRadius - (cornerRadius * (1 - bottomCornerValue))),
-                    y: rect.maxY - (cornerRadius - (cornerRadius * (1 - bottomCornerValue)))),
-            radius:
-                cornerRadius - (cornerRadius * (1 - bottomCornerValue)),
-            startAngle:
-                Angle(degrees: 90),
-            endAngle:
-                Angle(degrees: 180),
-            clockwise: false
-        )
-        
-        downPath.closeSubpath()
-        var topPath = Path()
-
-        topPath.addPath(downPath)
-        
-        var path3 = Path()
-        
-        path3.addPath(downPath)
-        
-        path3.intersection(topPath)
-        path3.addPath(topPath)
-        
-        return path3
-        
-    }
-    
-    public func inset(by amount: CGFloat) -> some InsettableShape {
-        var folder = self
-        folder.insetAmount += amount
-        return folder
-    }
-    
-    
-}
+//public struct Folder4: InsettableShape {
+//
+//    var topSizeModifier: CGFloat
+//    var insetAmount: CGFloat
+//    var bottomCornerValue: CGFloat
+//    
+//    public var animatableData: Double {
+//        get { bottomCornerValue }
+//        set { bottomCornerValue = newValue }
+//    }
+//    
+//    public init() {
+//        self.topSizeModifier = 0
+//        self.insetAmount = 0
+//        self.bottomCornerValue = 0
+//    }
+//    
+//    public init(topSizeModifier: CGFloat = 0, insetAmount: CGFloat = 0, bottomCornerValue: CGFloat = 0) {
+//        self.topSizeModifier = topSizeModifier
+//        self.insetAmount = insetAmount
+//        self.bottomCornerValue = bottomCornerValue
+//
+//    }
+//    
+//    public func path(in rect: CGRect) -> Path {
+//        
+//
+//        let referenceSize: CGFloat = 350
+//        let currentSize: CGFloat = (rect.width)
+//        let cornerRadius: CGFloat = 24 * (currentSize/referenceSize)
+//        let tabHeight: CGFloat = 48
+//        let tabCornerRadius: CGFloat = 18 * (currentSize/referenceSize)
+//        let textWidth: CGFloat = 82
+//        let begin = CGPoint(x: rect.minX, y: rect.maxY - cornerRadius)
+//
+//        var downPath = Path()
+//        downPath.move(to: begin)
+//        
+//        downPath.addArc(center: CGPoint(x: rect.minX + cornerRadius, y: rect.minY + cornerRadius * 3), radius: cornerRadius, startAngle: Angle(degrees: 180), endAngle: Angle(degrees: 270), clockwise: false)
+//        
+//        downPath.addArc(center: CGPoint(x: rect.maxX - cornerRadius*3 - textWidth, y: rect.minY + tabCornerRadius + cornerRadius/2), radius: tabCornerRadius, startAngle: Angle(degrees: 90), endAngle: Angle(degrees: 45), clockwise: true)
+//        
+//        downPath.addArc(center: CGPoint(x: rect.maxX - cornerRadius - textWidth, y: rect.minY + tabCornerRadius), radius: tabCornerRadius, startAngle: Angle(degrees: 225), endAngle: Angle(degrees: 270), clockwise: false)
+//
+//        downPath.addArc(center: CGPoint(x: rect.maxX - cornerRadius, y: rect.minY + cornerRadius), radius: cornerRadius, startAngle: Angle(degrees: 270), endAngle: Angle(degrees: 0), clockwise: false)
+//        
+//        downPath.addArc(
+//            center:
+//                CGPoint(
+//                    x:
+//                        rect.maxX - (cornerRadius - (cornerRadius * (1 - bottomCornerValue))),
+//                    y:
+//                        rect.maxY - (cornerRadius - (cornerRadius * (1 - bottomCornerValue)))),
+//            radius:
+//                cornerRadius - (cornerRadius * (1 - bottomCornerValue)),
+//            startAngle:
+//                Angle(degrees: 0),
+//            endAngle:
+//                Angle(degrees: 90),
+//            clockwise:
+//                false
+//        )
+//        
+//        downPath.addArc(
+//            center:
+//                CGPoint(
+//                    x: rect.minX + (cornerRadius - (cornerRadius * (1 - bottomCornerValue))),
+//                    y: rect.maxY - (cornerRadius - (cornerRadius * (1 - bottomCornerValue)))),
+//            radius:
+//                cornerRadius - (cornerRadius * (1 - bottomCornerValue)),
+//            startAngle:
+//                Angle(degrees: 90),
+//            endAngle:
+//                Angle(degrees: 180),
+//            clockwise: false
+//        )
+//        
+//        downPath.closeSubpath()
+//        var topPath = Path()
+//
+//        topPath.addPath(downPath)
+//        
+//        var path3 = Path()
+//        
+//        path3.addPath(downPath)
+//        
+//        path3.intersection(topPath)
+//        path3.addPath(topPath)
+//        
+//        return path3
+//        
+//    }
+//    
+//    public func inset(by amount: CGFloat) -> some InsettableShape {
+//        var folder = self
+//        folder.insetAmount += amount
+//        return folder
+//    }
+//    
+//    
+//}
 
 //Mudança de posição/tamanho da pasta supeior WIP
 public struct Folder5: InsettableShape {
