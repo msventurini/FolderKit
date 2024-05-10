268367010722f30a6811a7ffa91b6c9292a04f2b
msventurini <42521920+msventurini@users.noreply.github.com>
2024-05-10 04:50:15 -0300

    creating a new kind of folder

--- Sources/FolderKit/NewKoletoraDesignSystem/NewFolderShape.swift
+++ Sources/FolderKit/NewKoletoraDesignSystem/NewFolderShape.swift
@@ -1,6 +1,6 @@
 //
 //  SwiftUIView.swift
-//  
+//
 //
 //  Created by Matheus Silveira Venturini on 10/05/24.
 //
@@ -15,125 +15,129 @@ struct NewFolderShape: Shape {
     var rightPadding: CGFloat = 0
     var leftPadding: CGFloat = 0
     
-        func path(in rect: CGRect) -> Path {
-            
-            let referenceWidth: CGFloat = 109
-    
-            let currentSize: CGFloat = (rect.width)
-    
-            let cornerRadius: CGFloat = 8 * (currentSize/referenceWidth)
-            let tabCornerRadius: CGFloat = 6 * (currentSize/referenceWidth)
-            
-            let tabContentWidth: CGFloat = 22 * (currentSize/referenceWidth)
-            
-            
-            let minX = rect.minX + rightPadding
-            let maxX = rect.maxX - leftPadding
-            
-            let minY = rect.minY + topPadding
-            let maxY = rect.maxY - bottomPadding
-            
-            
-            
-            let begin = CGPoint(
-                x: minX,
-                y: rect.maxY - cornerRadius
-            )
-
-            let topLeftArcCenter = CGPoint(
+    func path(in rect: CGRect) -> Path {
+        
+        let referenceWidth: CGFloat = 109
+        
+        let currentSize: CGFloat = (rect.width)
+        
+        let cornerRadius: CGFloat = 8 * (currentSize/referenceWidth)
+        
+        let tabContentWidth: CGFloat = 22 * (currentSize/referenceWidth)
+        
+        
+        let minX = rect.minX + rightPadding
+        let maxX = rect.maxX - leftPadding
+        
+        let minY = rect.minY + topPadding
+        let maxY = rect.maxY - bottomPadding
+        
+        
+        
+        let begin = CGPoint(
+            x: minX,
+            y: rect.maxY - cornerRadius
+        )
+        
+        let topLeftArcCenter = CGPoint(
+            x: minX + cornerRadius,
+            y: minY + cornerRadius * 3
+        )
+        
+        let tabBottomArcCenter = CGPoint(
+            x: maxX - (cornerRadius * 3) - tabContentWidth,
+            y: minY + cornerRadius
+        )
+        
+        let tabTopArcCenter = CGPoint(
+            x: maxX - (cornerRadius*0.75) - tabContentWidth,
+            y: minY + cornerRadius
+        )
+        
+        
+        
+        var path = Path()
+        path.move(to: begin)
+        
+        
+        path.addArc(
+            center: topLeftArcCenter,
+            radius: cornerRadius,
+            startAngle: Angle(degrees: 180),
+            endAngle: Angle(degrees: 270),
+            clockwise: false
+        )
+        
+        path.addArc(
+            center: tabBottomArcCenter,
+            radius: cornerRadius,
+            startAngle: Angle(degrees: 90),
+            endAngle: Angle(degrees: 25),
+            clockwise: true
+        )
+        
+        path.addArc(
+            center: tabTopArcCenter,
+            radius: cornerRadius,
+            startAngle: Angle(degrees: -150),
+            endAngle: Angle(degrees: 270),
+            clockwise: false
+        )
+        
+        path.addArc(
+            center: CGPoint(
+                x: maxX - cornerRadius,
+                y: minY + cornerRadius),
+            radius: cornerRadius,
+            startAngle: Angle(degrees: 270),
+            endAngle: Angle(degrees: 0),
+            clockwise: false
+        )
+        
+        path.addArc(
+            center: CGPoint(
+                x: maxX - cornerRadius,
+                y: maxY - cornerRadius),
+            radius: cornerRadius,
+            startAngle: Angle(degrees: 0),
+            endAngle: Angle(degrees: 90),
+            clockwise: false
+        )
+        
+        path.addArc(
+            center: CGPoint(
                 x: minX + cornerRadius,
-                y: minY + cornerRadius * 2.85
-            )
-            
-            let tabBottomArcCenter = CGPoint(
-                x: maxX - (cornerRadius * 3) - tabContentWidth + 4,
-                y: minY + tabCornerRadius * 2
-            )
-            
-            var path = Path()
-            path.move(to: begin)
-            
-            
-            path.addArc(
-                center: topLeftArcCenter,
-                radius: cornerRadius,
-                startAngle: Angle(degrees: 180),
-                endAngle: Angle(degrees: 270),
-                clockwise: false
-            )
-            
-            path.addArc(
-                center: tabBottomArcCenter,
-                radius: tabCornerRadius/2,
-                startAngle: Angle(degrees: 90),
-                endAngle: Angle(degrees: 35),
-                clockwise: true
-            )
-            
-            path.addArc(
-                center: CGPoint(
-                    x: maxX - (tabCornerRadius) - tabContentWidth,
-                    y: minY + tabCornerRadius),
-                radius: tabCornerRadius,
-                startAngle: Angle(degrees: 210),
-                endAngle: Angle(degrees: 270),
-                clockwise: false
-            )
-
-            path.addArc(
-                center: CGPoint(
-                    x: maxX - cornerRadius,
-                    y: minY + cornerRadius),
-                radius: cornerRadius,
-                startAngle: Angle(degrees: 270),
-                endAngle: Angle(degrees: 0),
-                clockwise: false
-            )
-            
-            path.addArc(
-                center: CGPoint(
-                    x: maxX - cornerRadius,
-                    y: maxY - cornerRadius),
-                radius: cornerRadius,
-                startAngle: Angle(degrees: 0),
-                endAngle: Angle(degrees: 90),
-                clockwise: false
-            )
-            
-            path.addArc(
-                center: CGPoint(
-                    x: minX + cornerRadius,
-                    y: maxY - cornerRadius),
-                radius: cornerRadius,
-                startAngle: Angle(degrees: 90),
-                endAngle: Angle(degrees: 180),
-                clockwise: false
-            )
-            
-            
-            return path
-            
-        }
+                y: maxY - cornerRadius),
+            radius: cornerRadius,
+            startAngle: Angle(degrees: 90),
+            endAngle: Angle(degrees: 180),
+            clockwise: false
+        )
         
         
+        return path
         
-        
+    }
     
     
     
-//    .frame(minWidth: 150, idealWidth: 350, maxWidth: .infinity, minHeight: 100, idealHeight: 262 , maxHeight: .infinity, alignment: .center)
-
-//    func path(in rect: CGRect) -> Path {
-//        
-//        var path = Path()
-//
-//        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
-//        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
-//        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
-//        path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
-//
-//        return path
-//    }
+    
+    
+    
+    
+    //    .frame(minWidth: 150, idealWidth: 350, maxWidth: .infinity, minHeight: 100, idealHeight: 262 , maxHeight: .infinity, alignment: .center)
+    
+    //    func path(in rect: CGRect) -> Path {
+    //
+    //        var path = Path()
+    //
+    //        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+    //        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+    //        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+    //        path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
+    //
+    //        return path
+    //    }
     
     
     
@@ -147,19 +151,24 @@ struct testeNewShape2: View {
     var body: some View {
         
         ZStack {
-                
+            
             NewFolderShape()
                 .fill(.blue)
-//            NewFolderShape(topPadding: <#T##CGFloat#>, bottomPadding: <#T##CGFloat#>, rightPadding: <#T##CGFloat#>, leftPadding: <#T##CGFloat#>)
-//                .fill(.pink)
-             
             
-
-
-                
-
+            NewFolderShape(topPadding: 0, bottomPadding: 0, rightPadding: 0, leftPadding: 0)
+                .fill(.pink)
+            
+            
+            Image(.newFolderIconRef)
+                .opacity(0.3)
+            
+            
+            
+            
+            
         }
-        .ignoresSafeArea()
+        .frame(width: 109, height: 99)
+        //        .ignoresSafeArea()
         
     }
 }
@@ -173,24 +182,24 @@ struct testeNewShape: View {
     var body: some View {
         
         ZStack {
-                
+            
             NewFolderShape()
                 .fill(.blue)
             Folder(topSizeModifier: 0, insetAmount: 0)
                 .fill(.pink)
                 .opacity(0.2)
-//            VStack {
-//                Image(.newFolderIconRef)
-//                    .resizable()
-//                    .scaledToFit()
-//                    .opacity(0.2)
-//                Spacer()
-//            }
+            //            VStack {
+            //                Image(.newFolderIconRef)
+            //                    .resizable()
+            //                    .scaledToFit()
+            //                    .opacity(0.2)
+            //                Spacer()
+            //            }
             
-
-
-                
-
+            
+            
+            
+            
         }
         
     }
