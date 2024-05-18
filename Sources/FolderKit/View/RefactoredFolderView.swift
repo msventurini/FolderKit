3e173dcb06ff44d3ec9285a17a1d328477a5cbe8
msventurini <42521920+msventurini@users.noreply.github.com>
2024-05-18 05:08:18 -0300

    refactoring

--- Sources/FolderKit/View/RefactoredFolderView.swift
+++ Sources/FolderKit/View/RefactoredFolderView.swift
@@ -47,87 +47,163 @@ public struct RefactoredFolderView<Content: View, Header: View>: View{
     
     public var body: some View {
         
-        
-        Folder(topSizeModifier: 0)
-        
-            .fill(isClicked ? color : shadow)
-            .strokeBorder(style: .init(lineWidth: 2), antialiased: false)
-            .padding([.top,.leading], isClicked ? 0 : 8)
-        
-        
-        //            .ignoresSafeArea()
-            .overlay {
-                ZStack {
-                    
-                    GroupBox {
-                        if isClicked {
-                            content
-                                .transition(.move(edge: .bottom))
-                                .overlay {
-                                    VStack {
-                                        Rectangle()
-                                            .fill(LinearGradient(colors: [color, .clear], startPoint: .top, endPoint: .bottom))
-                                            .frame(height: 10)
-                                            .padding(.horizontal)
-                                        Spacer()
-                                    }
-                                }
-                            
-                        }
-                        
-                        
-                        
-                    } label: {
-                        header
-                            .padding(.bottom)
+        ZStack {
+            
+            
+            Folder(topSizeModifier: 0)
+                .fill(isClicked ? color : shadow)
+                .strokeBorder(style: .init(lineWidth: 2), antialiased: false)
+            
+                .padding([.top, .leading], isClicked ? 0 : 8)
+                .overlay {
+                    if showItems {
+                        content
+                            .transition(.identity)
                     }
-                    .groupBoxStyle(TesteGroupBox(folderIsOpen: $isClicked))
-                    .padding(.top, isClicked ? 76 : 0)
-                    //                    .padding()
-                    
-                    
-                    .ignoresSafeArea()
-                    
-                    
                 }
-                .mask(alignment: .center) {
-                    Rectangle()
-                        .frame(minWidth: 350, idealWidth: 350, maxWidth: .infinity)
-                }
-                
-                
-                Folder(topSizeModifier: 0)
-                    .fill(color ?? .cyan)
+            
+            VStack {
+                Spacer(minLength: 0)
+                    .layoutPriority(isClicked ? 1 : 0)
+                Folder(topSizeModifier: getTopFolderSpacer(fontSize: sizeCategory))
+//                    .fill(color)
+//                    .strokeBorder(style: .init(lineWidth: 2), antialiased: false)
+//                    .padding([.trailing, .bottom], (isClicked ? 0 : 8))
+//                    .padding([.top, .leading], (isClicked ? 8 : 0))
+//                    .frame(maxHeight: (isClicked ? 262 : .infinity))
+//                    .rotation3DEffect(Angle(degrees: (isClicked ? -90 : 0)), axis: (x: 1.0, y: 0.0, z: 0.0), anchor: .bottom, anchorZ: 0, perspective: 1)
+//                    .layoutPriority(isClicked ? 0 : 1)
+                    .fill(color)
                     .strokeBorder(style: .init(lineWidth: 2), antialiased: false)
                     .padding([.trailing, .bottom], (isClicked ? 0 : 8))
-                    .frame(maxHeight: (isClicked ? 0 : .infinity))
-                
-                    .visualEffect { content, geometryProxy in
-                        content
-                            .rotation3DEffect(Angle(degrees: (isClicked ? -90 : 0)), axis: (x: 1.0, y: 0.0, z: 0.0), anchor: .bottom, perspective: 1)
-                        
-                    }
-                
-                
-                
-                
-                
+                    .padding([.top, .leading], (isClicked ? 8 : 0))
+                    .frame(maxHeight: (isClicked ? 262 : .infinity))
+                    .rotation3DEffect(Angle(degrees: (isClicked ? -90 : 0)), axis: (x: 1.0, y: 0.0, z: 0.0), anchor: .bottom, anchorZ: 0, perspective: 1)
+                    .layoutPriority(isClicked ? 0 : 1)
                 
             }
-            .frame(minWidth: 150, idealWidth: 350, maxWidth: .infinity, minHeight: 100, idealHeight: 262 , maxHeight: .infinity, alignment: .center)
+            
+            
+            
+        }
+        
+//        Folder(topSizeModifier: 0)
+//        
+//            .fill(isClicked ? color : shadow)
+//            .strokeBorder(style: .init(lineWidth: 2), antialiased: false)
+//            .padding([.top,.leading], isClicked ? 0 : 8)
+//        
+//        
+//        //            .ignoresSafeArea()
+//            .overlay {
+//                ZStack {
+//                    
+//                    GroupBox {
+//                        if isClicked {
+//                            content
+//                                .transition(.move(edge: .bottom))
+//                                .overlay {
+//                                    VStack {
+//                                        Rectangle()
+//                                            .fill(LinearGradient(colors: [color, .clear], startPoint: .top, endPoint: .bottom))
+//                                            .frame(height: 10)
+//                                            .padding(.horizontal)
+//                                        Spacer()
+//                                    }
+//                                }
+//                            
+//                        }
+//                        
+//                        
+//                        
+//                    } label: {
+//                        header
+//                            .padding(.bottom)
+//                    }
+//                    .groupBoxStyle(TesteGroupBox(folderIsOpen: $isClicked))
+//                    .padding(.top, isClicked ? 76 : 0)
+//                    //                    .padding()
+//                    
+//                    
+//                    .ignoresSafeArea()
+//                    
+//                    
+//                }
+//                .mask(alignment: .center) {
+//                    Rectangle()
+//                        .frame(minWidth: 350, idealWidth: 350, maxWidth: .infinity)
+//                }
+//                
+//                
+//                Folder(topSizeModifier: 0)
+//                    .fill(color ?? .cyan)
+//                    .strokeBorder(style: .init(lineWidth: 2), antialiased: false)
+//                    .padding([.trailing, .bottom], (isClicked ? 0 : 8))
+//                    .frame(maxHeight: (isClicked ? 0 : .infinity))
+//                
+//                    .visualEffect { content, geometryProxy in
+//                        content
+//                            .rotation3DEffect(Angle(degrees: (isClicked ? -90 : 0)), axis: (x: 1.0, y: 0.0, z: 0.0), anchor: .bottom, perspective: 1)
+//                        
+//                    }
+//                
+//                
+//                
+//                
+//                
+//                
+//            }
+        .frame(minWidth: 150, idealWidth: 350, maxWidth: .infinity, minHeight: 100, idealHeight: 262 , maxHeight: .infinity, alignment: .center)
         //            .ignoresSafeArea()
         
         
-            .onChange(of: isClicked) { oldValue, newValue in
-                withAnimation(.linear) {
-                    if newValue == true && animationOnProgress == true {
-                        showItems = true
-                    } else {
-                        showItems = isClicked && !animationOnProgress
-                    }
-                }
-            }
+//            .onChange(of: isClicked) { oldValue, newValue in
+//                withAnimation(.linear) {
+//                    if newValue == true && animationOnProgress == true {
+//                        showItems = true
+//                    } else {
+//                        showItems = isClicked && !animationOnProgress
+//                    }
+//                }
+//            }
         
+    }
+    
+    
+    func getTopFolderSpacer(fontSize: DynamicTypeSize) -> Double {
+        
+        switch fontSize {
+        case .xSmall:
+            12
+        case .small:
+            16
+        case .medium:
+            20
+        case .large:
+            22
+        case .xLarge:
+            30
+        case .xxLarge:
+            34
+        case .xxxLarge:
+            42
+        case .accessibility1:
+            60
+        case .accessibility2:
+            78
+        case .accessibility3:
+            98
+        case .accessibility4:
+            120
+        case .accessibility5:
+            140
+            
+        @unknown default:
+            fatalError()
+        }
+        
+        
+        //        reeturn 0
     }
     
     
