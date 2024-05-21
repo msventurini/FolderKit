95f92dfd0d06645b6e08b1b483be03840531324a
msventurini <42521920+msventurini@users.noreply.github.com>
2024-05-21 03:21:48 -0300

    refactoring

--- Sources/FolderKit/NewKoletoraDesignSystem/NewClosedFolderView.swift
+++ Sources/FolderKit/NewKoletoraDesignSystem/NewClosedFolderView.swift
@@ -14,19 +14,30 @@ public struct NewClosedFolderView: View{
     let text: String?
     let color: Color
     let shadow: Color
+    
+    
+    
     @State var propertiesTextOpacity: CGFloat
     @State private var showItems: Bool = false
+    
+    
     
     @ScaledMetric(relativeTo: .body) var topSizeModifier = 50
     @Environment(\.dynamicTypeSize) private var sizeCategory
     
-    public init(text: String? = nil, color: Color, shadow: Color, propertiesTextOpacity: CGFloat = 0, topSizeModifier: Int = 50) {
+    @Binding var isOnPerspective: Bool
+
+    
+    public init(text: String? = nil, color: Color, shadow: Color, propertiesTextOpacity: CGFloat = 0, topSizeModifier: Int = 50, isOnPerspective: Binding<Bool>) {
         self.text = text
         self.color = color
         self.shadow = shadow
         self.propertiesTextOpacity = propertiesTextOpacity
+        self._isOnPerspective = isOnPerspective
         
     }
+    
+    
     
     public var body: some View {
         
@@ -40,7 +51,8 @@ public struct NewClosedFolderView: View{
                 NewFolderShape()
                     .fill(color)
                     .strokeBorder(style: .init(lineWidth: 2), antialiased: true)
-                    .padding([.trailing, .bottom], (8))
+                    .padding([.top,.leading], (isOnPerspective ? 0 : 8))
+                    .padding([.trailing, .bottom], (isOnPerspective ? 8 : 0))
                     .frame(maxHeight: (.infinity))
                 
             }
@@ -55,7 +67,7 @@ public struct NewClosedFolderView: View{
 
 
 #Preview {
-    NewClosedFolderView(color: .cyan, shadow: .blue)
+    NewClosedFolderView(color: .cyan, shadow: .blue, isOnPerspective: .constant(true))
         .frame(width: 300, height: 400)
         .background(.yellow)
 }
